8f249231844f40aba028d0086bfba5bae8d6ea0d
Arsen <Arsen@developer.arsenmkhitaryan@gmail.com>
2024-02-23 12:55:32 +0400

    2 tasks are done

---
+++ tasks.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+
+int main () {
+
+    int num = 10;
+    char sym = 'a';
+
+    float num2 = 25.5;
+    double num3 = 22.2;
+
+    bool status = true;
+    std::cout<< "int is : "    << num << std::endl;
+    std::cout<< "sym is : "    << sym << std::endl;
+    std::cout<< "float is : "  << num2 << std::endl;
+    std::cout<< "double is : " << num3 << std::endl;
+    std::cout<< "bool is : "   << status << std::endl;
+    return 0;
+}
