46b5d58ff5016b8ee0487b2b56efe3715a380eed
Arsen Mkhitaryan <developer.arsenmkitaryan@gmail.com>
2024-02-23 22:41:26 +0400

    done function pointer task

--- day3/task1.cpp
+++ day3/task1.cpp
@@ -6,7 +6,7 @@ void swap(int * a, int * b) {
     *b = tmp;
 }
 
-void bubbleSort(int * arr, int size) {
+void bubbleSort(int * arr, size_t size) {
     for (int step = 0; step < size; ++step) {
         for (int i = 0; i < size - step; ++i) {
             if (arr[i] > arr[i + 1]) {
@@ -16,7 +16,7 @@ void bubbleSort(int * arr, int size) {
     }
 }
 
-void selectionSort(int * arr, int size) {
+void selectionSort(int * arr, size_t size) {
   for (int step = 0; step < size - 1; step++) {
     int min_idx = step;
     for (int i = step + 1; i < size; i++) {
@@ -27,8 +27,34 @@ void selectionSort(int * arr, int size) {
   }
 }
 
+void printArr(int * arr, size_t size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i];
+    }
+    std::cout << std::endl;
+}
+
 int main () {
     const int size = 10;
     int arr[] = {1,5,3,4,7,8,5,6,9,8};
-    void * ptr = NULL;
+    
+    typedef void (*Sort) (int *, size_t);
+    Sort sort = NULL;
+    
+    enum Sorting {
+        BUBLE = 1,
+        SELECTION
+    };
+
+    char selected = 0;
+    std::cout << "Please enter your selection type" << std::endl << "1: Buble" << std::endl << "2: Selection" << std::endl;
+    std::cin >> selected; 
+    
+    if (selected == BUBLE) { sort = bubbleSort;}
+    else { sort = selectionSort; }
+
+    sort(arr, size);
+    printArr(arr, size);
+
+    return 0;
 }
---
+++ day3/task2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+struct Date {
+    int day;
+    int month;
+    int year;
+};
+
+enum WeekDays {
+    MONDAY = 1,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+};
+
+void printDate(WeekDays weekDays, Date date) {
+
+}
+
+int main () {
+    Date today = {23, 2, 2024};
+
+    // printDate();
+}
