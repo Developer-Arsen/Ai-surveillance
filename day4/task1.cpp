e1c25d3ca4fdb10aac12fee70cb35d8d73dec58f
Arsen <Arsen@developer.arsenmkhitaryan@gmail.com>
2024-03-01 10:56:38 +0400

    wokring with files

--- day4/task1.cpp
+++ day4/task1.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 int main() {
-    std::string names[] = {"arsen", "arman"};
-    
+    std::vector<std::string> names = {"arsen", "arman"};
     std::ofstream outputFile("example.txt");
+
     if (!outputFile) {
-        std::cerr << "Error: Unable to open the file." << std::endl;
-        return 1; 
+        std::cerr << "Error: Unable to open file" << std::endl;
+        return 1;
     }
-   
-    for (const auto& name : names) {
-        outputFile << name << std::endl;
+
+    for (int i = 0; i < names.size(); i++) {
+        outputFile << names[i] << std::endl;
     }
+    // for (const auto&name : names) {
+    //     outputFile << name << std::endl;
+    // }
 
     outputFile.close();
 
     std::ifstream inputFile("example.txt");
-    if (!inputFile) {
-        std::cerr << "Error: Unable to open the file." << std::endl;
-        return 1; 
+    if (!inputFile)
+    {
+       std::cerr << "Error: unable to open file" << std::endl;
+       return 1;
     }
 
     std::string line;
-    std::cout << "Contents of the file:" << std::endl;
-    while (std::getline(inputFile, line)) {
+    while (std::getline(inputFile, line))
+    {
         std::cout << line << std::endl;
     }
-
-    return 0; 
+    
+    return 0;
 }
